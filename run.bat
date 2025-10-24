b1f7e8dc79f509d4b9a3b05bb3edb4e16dd76733
Cameron Beck <cameron.b.beck@outlook.com>
2025-10-24 12:02:32 -0500

    Deleted redundant directory and created readme

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-python C:\Users\camer\OneDrive\Documents\Side Projects\Python\Web Scraper\Scraper.py REM This needs to be changed to whatever your file path is
+python C:\Users\camer\OneDrive\Documents\Side Projects\Python\Job-Scraper\Scraper.py REM This needs to be changed to whatever your file path is
 pause
