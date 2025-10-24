39e1f899e80997ef39d051924e5d85f3b97dd372
Cameron Beck <cameron.b.beck@outlook.com>
2025-10-24 13:16:49 -0500

    Changed architectural idea. Moving to a 3 file architecture that will be executed on another batch file. run.bat will be updated in the future

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-python "C:\Users\camer\OneDrive\Documents\Side Projects\Python\Job-Scraper\Scraper.py" REM Change to your file path
+python "C:\Users\camer\OneDrive\Documents\Side Projects\Python\Job-Scraper\IndeedScraper.py" REM Change to your file path
 pause
